d5faa58c070b60f49aa3c7e32baaa1841f99b79b
@NicBeans <nicholasbosman007@gmail.com>
2023-07-24 13:21:25 +0200

    testing new stop

--- commands/memes/stop.js
+++ commands/memes/stop.js
@@ -2,23 +2,23 @@ const { SlashCommandBuilder } = require("discord.js");
 const { exec } = require("child_process");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("stop")
-        .setDescription("stop tha shit"),
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("stop the bot"),
 
-    async execute(interaction) {
-        try {
-            exec("pkill -f startthatshit.js", (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-            });
-            await interaction.reply("shi stopped.");
-        } catch (error) {
-            console.log("[ERROR]", error);
+  async execute(interaction) {
+    try {
+      exec(`kill -2 $(pgrep -f startthatshit.js)`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
         }
-    },
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+      await interaction.reply("Bot stopped.");
+    } catch (error) {
+      console.log("[ERROR]", error);
+    }
+  },
 };
